f24ff9a02e4c4575b32ad89c7b4c8d9c40844a30
Nikoleta Verbeck <nerdynick@gmail.com>
2020-07-30 12:30:27 -0600

    More debug

--- travis/build-master.sh
+++ travis/build-master.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+set -o xtrace
 ls -l ./*
 PWD=`pwd`
-docker run purbon/kafka-topology-builder /bin/sh \
+docker run
     -a \
     -c "ls -l ./*; kafka-topology-builder.sh  --clientConfig /opt/build/topology-builder.properties --topology /opt/build/topologies --brokers $CLUSTER_BOOTSTRAP" \
-    --device=$PWD:/opt/build
+    --device=$PWD:/opt/build \
+    purbon/kafka-topology-builder /bin/sh
