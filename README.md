691d0f0de382dbbacd6ec2b8dda38a8483307bfc
Nikoleta Verbeck <nerdynick@gmail.com>
2020-07-30 11:35:32 -0600

    Updated Build Script and README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # kafka-topology-builder-travisci-demo
+
+![Build Status](https://travis-ci.com/nerdynick/kafka-topology-builder-travisci-demo.svg?branch=master)
+
 A variant of the Kafka Topology Builder Jenkins Demo, but leveraging Travis CI.
--- travis/build-master.sh
+++ travis/build-master.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker run purbon/kafka-topology-builder /bin/sh \
     -a \
-    -c "kafka-topology-builder.sh  --clientConfig /opt/build/topology-builder.properties --topology /opt/build/topologies" \
+    -c "kafka-topology-builder.sh  --clientConfig /opt/build/topology-builder.properties --topology /opt/build/topologies --brokers $CLUSTER_BOOTSTRAP" \
     --device=./:/opt/build
