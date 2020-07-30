4feedb3344e58e4af939c900afba481906890079
Nikoleta Verbeck <nerdynick@gmail.com>
2020-07-30 12:25:52 -0600

    Attempts to fix docker mounting

--- travis/build-master.sh
+++ travis/build-master.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+ls -l ./*
+PWD=`pwd`
 docker run purbon/kafka-topology-builder /bin/sh \
     -a \
-    -c "ls -lr ./*; kafka-topology-builder.sh  --clientConfig /opt/build/topology-builder.properties --topology /opt/build/topologies --brokers $CLUSTER_BOOTSTRAP" \
-    --device=./:/opt/build
+    -c "ls -l ./*; kafka-topology-builder.sh  --clientConfig /opt/build/topology-builder.properties --topology /opt/build/topologies --brokers $CLUSTER_BOOTSTRAP" \
+    --device=$PWD:/opt/build
